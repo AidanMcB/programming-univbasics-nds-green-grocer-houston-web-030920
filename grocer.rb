48b586fe5668f03bd1d8154a955f2dc92bce6a29
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-04 18:56:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,31 +1,19 @@
 def increment_count_of_item (cart, item_name)
-  i = 0
-  while i < cart.size do
-    current_item = cart[i]
-    if ( current_item[:item] == item_name )
-      current_item[:count] += 1
-    end
-    i += 1
-  end
-  cart
+
 end
 
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
-  # Consult README for inputs and outputs
-
-  i = 0
-
-  while i < collection.size do
-    current_item = collection[i]
-    if ( current_item[:item] == name )
-      return current_item
-    end
-    i += 1
+  # Consult README for inputs and output
+ i = 0 
+ while i < collection.length do 
+  if collection[i][:item] == name 
+    return collection[i]
   end
-  nil
-
+  i += 1 
+  end
+  return nil
 end
 
 def consolidate_cart(cart)
@@ -33,63 +21,38 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  
-  newArr = [] 
- i = 0
-
-  while i < cart.size do
-    current_item = cart[i]
-    if ( find_item_by_name_in_collection( current_item[:item], newArr ) == nil)
-      current_item[:count] = 1
-      newArr.push(current_item)
-    else
-      increment_count_of_item( newArr, current_item[:item] )
-    end
-    i += 1
+  new_cart =[] 
+  i = 0                             #start a counter
+  while i < cart.length do          #iterate through the cart   
+      new_cart_item = find_item_by_name_in_collection(cart[i][:item], new_cart)  #returns item OR nil
+    if new_cart_item                                                              #if new_cart_item != nil
+      new_cart_item[:count] += 1            #value of count key +1
+    else 
+      new_cart_item = {
+      :item => cart[i][:item],
+      :price => cart[i][:price],
+      :clearance => cart[i][:clearance],
+      :count => 1
+      }
+      new_cart << new_cart_item 
+    end 
+    i += 1                          
   end
-  newArr
-
+  new_cart
 end
+#item: [{:item => "Avocado, :price => 3.00, :clearance =>, counter => 3}]
+
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-   i = 0
-
-  while i < coupons.size do
-    current_coupon = coupons[i]
-    discount = find_item_by_name_in_collection( current_coupon[:item], cart )
-      if ( discount[:count] / current_coupon[:num] >= 1 )
-        cart.push( {:item => "#{current_coupon[:item]} W/COUPON",
-                    :price => (current_coupon[:cost] / current_coupon[:num]).round(2),
-                    :clearance => discount[:clearance],
-                    :count => discount[:count] - ( discount[:count] % current_coupon[:num])})
-
-        discount[:count] %= current_coupon[:num]
-      end
-    i += 1
-  end
-  cart
-
+  #cart = array collection of item hashes 
+  #coupons = 
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-    i = 0
-  newArr = []
 
-  while i < cart.size do
-    current_item = cart[i]
-    if ( current_item[:clearance] )
-      current_item[:price] = current_item[:price] - ( current_item[:price] * 0.20 )
-    end
-    newArr.push( current_item )
-    i += 1 
-  end
-  newArr
 end
 
 def checkout(cart, coupons)
@@ -102,21 +65,6 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-    checkout = consolidate_cart( cart )
-  checkout = apply_coupons( checkout, coupons )
-  checkout = apply_clearance( checkout )
 
-  i = 0
-  total = 0
 
-  while i < checkout.size do
-    current_item_total = checkout[i][:price] * checkout[i][:count]
-    current_item_total.round(2)
-    total += current_item_total
-    i += 1
-  end
-  if ( total > 100 )
-    total *= 0.90
-  end
-  total
 end
